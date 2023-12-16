8353ede074007167a050b1a8c6b930298349004d
Ali Pourgheysari <a.geysar@gmail.com>
2023-12-16 18:59:57 +0330

    Create landingPageModel.cs

---
+++ Ticketing.Core/Models/landingPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Core.Models
+{
+    public class landingPageModel
+    {
+        public string? section1 { get; set; }
+        public string? section2 { get; set; }
+    }
+}
