0f9d6a2033463e79a494ad46b6f7948d8d7315df
Ali Pourgheysari <a.geysar@gmail.com>
2023-12-16 19:32:20 +0330

    Create LandingMapperExtensions.cs

---
+++ Ticketing.Core/EntityMappings/LandingMapperExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketing.Core.Entities;
+using Ticketing.Core.Models;
+
+namespace Ticketing.Core.EntityMappings
+{
+    public static class LandingMapperExtensions
+    {
+        public static List<LandingPageModel> ToLandingPageModellList(this List<LandingPage> entities)
+        {
+            var result = new List<LandingPageModel>();
+            if (entities?.Any() ?? false)
+            {
+                foreach (var entity in entities)
+                {
+                    result.Add(new LandingPageModel
+                    {
+                        section1 = entity.Section1,
+                        section2 = entity.Section2
+                    });
+                }
+            }
+            return result;
+        }
+    }
+}
