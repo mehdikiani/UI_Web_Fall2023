03946412b15007d193371175a8eb876160a54956
Ali Pourgheysari <a.geysar@gmail.com>
2023-12-22 18:16:03 +0330

    Landing page service

---
+++ Ticketing.Services/ILandingPageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketing.Core.Entities;
+
+namespace Ticketing.Services
+{
+    public interface ILandingPageService
+    {
+        public Task<List<LandingPage>> LoadRandomSectionsAsync(int count);
+    }
+}
---
+++ Ticketing.Services/LandingPageService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketing.Core.Entities;
+using Ticketing.Data;
+
+namespace Ticketing.Services
+{
+    public class LandingPageService : ILandingPageService
+    {
+        private readonly IRepository<LandingPage> _repository;
+
+        public LandingPageService(IRepository<LandingPage> repository)
+        {
+            this._repository = repository;
+        }
+        public async Task<List<LandingPage>> LoadRandomSectionsAsync(int count)
+        {
+            return await _repository.GetRandomRow(count).ToListAsync();
+        }
+    }
+}
